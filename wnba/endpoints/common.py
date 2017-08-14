863c537a14d1ecff7776cb5e6c1e9e2c2a09f123
Rory Cole <rory.cole1990@gmail.com>
2017-08-14 15:41:46 +0100

    Add common parsing

--- wnba/endpoints/common.py
+++ wnba/endpoints/common.py
@@ -1,4 +1,6 @@
 
+import pandas as pd
+
 from wnba.endpoints.baseendpoint import BaseEndpoint
 
 
@@ -7,14 +9,17 @@ class Common(BaseEndpoint):
     def current_players(self, season):
         url = r'%s%s/players/10_player_info.json' % (self.client.data_url, season)
         r = self.request(url, self.client.data_headers)
-        return r
+        df = pd.DataFrame(r.get('pls', {}).get('pl', []))
+        return df
 
     def historic_players(self, season):
         url = r'%s%s/players/10_historical_players.json' % (self.client.data_url, season)
         r = self.request(url, self.client.data_headers)
-        return r
+        df = pd.DataFrame(r.get('pls', {}).get('pl', []))
+        return df
 
     def current_teams(self, season):
         url = r'%s%s/teams/10_team_info.json' % (self.client.data_url, season)
         r = self.request(url, self.client.data_headers)
-        return r
+        df = pd.DataFrame(r.get('tms', {}).get('t', []))
+        return df
